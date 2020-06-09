b31f380a561c604431cce77e453d5f14cc0390b3
Onipede Adedayo Samson <50950766+onipedadedayo142@users.noreply.github.com>
2020-06-09 17:50:55 +0100

    alert

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('form').onsubmit=function () {
-	alert('Thank you for reaching out to me, message delivered, i will get back to you shortly.')
+	alert(Thank you for reaching out to me, message delivered, i will get back to you shortly.)
 }
